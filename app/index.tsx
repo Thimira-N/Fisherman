eefc25708b897241cdc9062c0a2bb985b6b51a55
Thimira Navodana <thimiranavodana2002@gmail.com>
2025-04-24 23:31:08 +0530

    index

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+        <Text className="text-3xl text-blue-500 font-bold">Welcome to the</Text>
+        <Text className="text-7xl text-blue-800 font-extrabold">FISHERMAN!</Text>
+        <Text className="text-2xl text-black italic">we catch | we eat | repeat</Text>
     </View>
   );
 }
